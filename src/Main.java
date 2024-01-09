545b7d40a117bde1d44c45bec72b585f3d4ce2cd
RayaanUddin <rayaan.uddin@outlook.com>
2024-01-09 20:58:41 +0000

    Implemented a rough form with a label, button and textbox. Made the text box and button functional.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -5,6 +8,27 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static JFrame frameMain;
+    static ClientToServer serverConnection;
+    private JTextField textMessage;
+    private JButton buttonSend;
+    private JPanel panelMain;
+    private JLabel labelMessages;
+
+    public void newMessage(String message) {
+        labelMessages.setText(labelMessages.getText() + "\n" + message);
+    }
+
+    public Main() {
+        buttonSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                serverConnection.sendToServer(textMessage.getText());
+                textMessage.setText("");
+            }
+        });
+    }
+
     public static void main(String[] args) {
         Socket serverSocket;
         Scanner scanner = new Scanner(System.in);
@@ -14,10 +38,16 @@ public class Main {
             System.out.println("Unable to connect to server");
             return;
         }
-        ClientToServer serverConnection = new ClientToServer(serverSocket);
+        serverConnection = new ClientToServer(serverSocket);
         serverConnection.start();
-        while (true) {
-            serverConnection.sendToServer(scanner.nextLine());
-        }
+        frameMain = new JFrame("Main");
+        frameMain.setVisible(true);
+        Main main = new Main();
+        frameMain.setContentPane(main.panelMain);
+        main.labelMessages.setText("Hello World!");
+        frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //while (true) {
+            //serverConnection.sendToServer(scanner.nextLine());
+        //}
     }
 }
