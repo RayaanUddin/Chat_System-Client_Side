2cfd808aabe7130a3fae9349b4ce2793ad20944f
RayaanUddin <rayaan.uddin@outlook.com>
2024-03-03 03:32:43 +0000

    New Packet Version 1.2. Can hold multiple values for commands, etc.

--- src/Packet.java
+++ src/Packet.java
@@ -5,46 +5,75 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Packet class is used to hold data being transmitted to clients from server. Can either contain a message or a command.
+ * @version 1.2
+ */
 public class Packet implements Serializable {
     final private ClientDetails senderDetails;
 
     private String type; // message or command, message on default
 
-    final private String message; // used to hold message or command string
+    final private String[] args;
 
     final private LocalDate date;
 
     final private LocalTime time;
 
-    public Packet(String message, ClientDetails senderDetails) {
+    /**
+     * Constructor for a packet. Sends any information related to transmission of messages or commands between clients and server.
+     * Type is set to message by default. If the packet is a command, it can be set using the setAsCommand method.
+     * @param senderDetails details of the client sending the command/ message.
+     * @param args the command/ message arguments. Holds any values, such as the message, command values, recipient connection id etc.
+     * @see #setAsCommand()
+     * @return null
+     */
+    public Packet(ClientDetails senderDetails, String[] args) {
         this.senderDetails = senderDetails;
-        this.message = message;
         this.date = LocalDate.now();
         this.time = LocalTime.now();
+        this.args = args;
         type = "message";
     }
 
-    public String getMessage() {
-        return message;
+    public String[] getArguments() {
+        return args;
     }
 
     public String getDateTime() {
         return date.format(DateTimeFormatter.ofPattern("dd-MM")) + " " + time.getHour() + ":" + time.getMinute();
     }
 
-    public void setAsCommand() {
-        this.type = "command";
+    public Packet setAsCommand() {
+        if (args.length > 0) {
+            this.type = "command";
+        } else {
+            System.out.println("Error: Command packet has no arguments");
+        }
+        return this;
     }
 
     public ClientDetails getClientDetails() {
         return senderDetails;
     }
 
-    public boolean isMessage() {
-        return type.equalsIgnoreCase("message");
+    public boolean type(String type) {
+        return this.type.equalsIgnoreCase(type.toLowerCase()); // Not case-sensitive
     }
 
+    /**
+     * Used to print out the message to user interface
+     * @return String | null
+     */
     public String toString() {
-        return senderDetails.getName() + "[" + senderDetails.getConnectionId() + "]: " + message + "    " + getDateTime();
+        if (type("message")) {
+            if (args.length > 1) {
+                return senderDetails.getName() + "[" + senderDetails.getConnectionId() + ":private] : " + args[1] + "    " + getDateTime();
+            } else {
+                return senderDetails.getName() + "[" + senderDetails.getConnectionId() + "]: " + args[0] + "    " + getDateTime();
+            }
+        } else {
+            return null;
+        }
     }
 }
